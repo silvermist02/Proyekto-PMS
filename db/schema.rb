03675dc6445f3598475dd83d9ed736969612266e
Jesse <jesse_silvermist02@yahoo.com.ph>
2011-06-16 17:22:49 +0800

    final tickets edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110616015106) do
+ActiveRecord::Schema.define(:version => 20110616091141) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -65,10 +65,21 @@ ActiveRecord::Schema.define(:version => 20110616015106) do
     t.datetime "updated_at"
   end
 
-  create_table "tags", :force => true do |t|
-    t.string   "name"
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context"
     t.datetime "created_at"
-    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
   end
 
   create_table "tickets", :force => true do |t|
@@ -91,14 +102,6 @@ ActiveRecord::Schema.define(:version => 20110616015106) do
   add_index "tickets", ["priority"], :name => "index_tickets_on_priority"
   add_index "tickets", ["project_id"], :name => "index_tickets_on_project_id"
   add_index "tickets", ["status"], :name => "index_tickets_on_status"
-
-  create_table "tickets_tags", :id => false, :force => true do |t|
-    t.integer "ticket_id"
-    t.integer "tag_id"
-  end
-
-  add_index "tickets_tags", ["tag_id"], :name => "index_tickets_tags_on_tag_id"
-  add_index "tickets_tags", ["ticket_id"], :name => "index_tickets_tags_on_ticket_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
